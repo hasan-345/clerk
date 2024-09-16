3421e13ebcb0346c7cc5ec743ddb6ab59d752bb2
hasan-345 <ahtishamhassan345@gmail.com>
2024-09-15 23:43:49 -0700

    changes

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,14 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import {Webhook} from "svix"
+import { createUser } from "@/actions/user.action";
+
+export async function POST(req: Request){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
